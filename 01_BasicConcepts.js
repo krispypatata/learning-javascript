173140b4c648178e2021d218c2ee1c7c60b014fa
krispypatata <ksgabinete@up.edu.ph>
2024-03-11 12:55:43 +0800

    started learning javascript :)

---
+++ 01_BasicConcepts.js
@@ -0,0 +1,15 @@
+console.log('Hellio World');
+
+var a;
+var b;
+var c;
+
+var age = 20;
+
+if (age >= 18){
+    console.log("Can vote on May 13, 2019");
+}
+
+else{
+    console.log("Cannot vote on May 13, 2019");
+}
